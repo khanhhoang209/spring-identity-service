1609ab2861d032a3287f0f5984da61381232a765
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-12-21 08:29:08 +0700

    config swagger

--- src/main/java/com/fpt/identityservice/config/SwaggerConfig.java
+++ src/main/java/com/fpt/identityservice/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.fpt.identityservice.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public io.swagger.v3.oas.models.OpenAPI swaggerOpenAPI() {
-        return new io.swagger.v3.oas.models.OpenAPI().info( new Info()
+    public OpenAPI swaggerOpenAPI() {
+        return new OpenAPI().info( new Info()
                 .title("spring-identity-service")
                 .version("v1")
         );
