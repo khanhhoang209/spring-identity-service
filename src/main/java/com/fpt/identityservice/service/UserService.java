a7473105ccd76193d9ac5d213594b55ccfbd57ef
khanhhoang209 <lehoangkhanh209@gmail.com>
2024-12-23 23:12:42 +0700

    config application.yaml

--- src/main/java/com/fpt/identityservice/service/UserService.java
+++ src/main/java/com/fpt/identityservice/service/UserService.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 
